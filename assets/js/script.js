2ac5b9fb2b91cbf0c1d5dc2781d0a149ebaff22c
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-10-23 17:18:02 +0200

    add variable for API

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,9 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 //dichiaro una variabile per l'elemento html
 const emailListEl = document.getElementById('email-list')
+console.log(emailListEl);
+
+//creo una variabile in cui inserire l'url dell'API
+const randomEmailUrl = 'https://flynn.boolean.careers/exercises/api/random/mail'
+console.log(randomEmailUrl);
+
