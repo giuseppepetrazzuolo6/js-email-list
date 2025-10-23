3a62b97b31370dde42e28fc627c10ceb0ec4fb6d
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-10-23 17:15:48 +0200

    created variable for the html element and change title in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,6 @@ Attraverso l'apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
+
+//dichiaro una variabile per l'elemento html
+const emailListEl = document.getElementById('email-list')
